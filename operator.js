beaa28440ee7e8472b822a9beac06a5f2a53f373
Ishanjain2 <126861790+Ishanjain2@users.noreply.github.com>
2024-12-05 10:30:48 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,24 @@
+let score="Ishan ";
+console.log(typeof score);
+console.log(typeof (score));
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber );
+ console.log( valueInNumber );
+  let isloggedin="";
+  let BooleanIsloggedIn= Boolean(isloggedin);
+  console.log( BooleanIsloggedIn);
+let somenumber = 33 ;
+let Stringnumber= String("somenumber")
+console.log(somenumber)
+let value = 5 
+ let negvalue = -value 
+let posvalue = +value
+console.log(negvalue);
+console.log(posvalue);
+let str1 = "hello"
+let str2 = "ishan"
+str3 = str1 + str2
+console.log(str3);
+console.log("2" + 3);
+console.log(1 + "2");
+
